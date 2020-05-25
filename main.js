bdeb6a3fa38ab4fd3877a5dbe56cb723e07c99b9
ivytran <59583454+ivytran@users.noreply.github.com>
2020-05-24 19:53:09 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,12 @@
 // Now comes the code that must wait to run until the document is fully loaded
 document.addEventListener("DOMContentLoaded", function (event) {
 
-  
+function getOption(){
+    selectElement =
+        document.querySelector('invItems');
+    output = selectElement.Value;
+    document.querySelector('.output').textContent=output;
+}
 });  
 
 
